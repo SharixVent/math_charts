c68193c2fc0a8ee59bfc94fd617ade775b382a22
Dawid Krawczyk <dawidkrawczyk@MacBook-Pro-Cocon.local>
2025-07-02 15:09:53 +0200

    Added Fields.tsx and Fields.css as handler

---
+++ src/components/fieldsData/Fields.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Field from "../field/Field.tsx";
+
+export const FieldsData: React.FC = () => {
+
+    const callback = (value: number) => {
+        console.log(value)
+    }
+
+
+    return (
+        <>
+        
+        </>
+    );
+};
+
+
+export default FieldsData;
