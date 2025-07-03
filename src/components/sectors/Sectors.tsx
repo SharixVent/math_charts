2ff96e7be37b1b4033ec8dba125ae2078b22aa45
Dawid Krawczyk <dawidkrawczyk@MacBook-Pro-Cocon.local>
2025-07-03 13:46:02 +0200

    Added Sectors.tsx and styles

--- src/components/sectors/Sectors.tsx
+++ src/components/sectors/Sectors.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Canvas_2D from '../canva-2d/Canva_2D';
 import Canvas_3D from '../canva-3d/Canva_3D'; 
 import SwitchButton from '../buttons/switch/Switch';
-import Field from '../field/Field';
+import FieldsData from '../fieldsData/Fields';
 import Popup from '../popup/Popup';
+import './Sectors.css'
 
 export const Sectors: React.FC = () => {
 
+    const [formJson, setFormJson] = useState<{ [key: string]: any }>({});
     const [switchT, setSwitchT] = useState<"2D" | "3D">("2D");
     const [showPopup, setShowPopup] = useState(false);
-    const [data, setData] = useState(
-            Array.from({ length: 100 }, (_,index) => {
-            const x: number = index * 0.1;
-            const y: number = Math.sin(x);
-            return {x, y}
-            })
-        )
+    const [data, setData] = useState<{x: number; y: number}[]>();
+    useEffect(() => {
+        const length = Number(formJson.range) || 100;
+        const newData = Array.from({ length }, (_, index) => {
+            const x = index * 0.1;
+            const y = Math.sin(x);
+            return { x, y };
+        });
+        setData(newData);
+    }, [formJson]);
 
     const handleConfirmSwitch = () => {
         setSwitchT(prev => prev === '2D' ? "3D" : "2D");
@@ -30,6 +35,16 @@ export const Sectors: React.FC = () => {
         setShowPopup(false);
     };
 
+    function handleSumbit(e:React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+
+        const formJson = Object.fromEntries(formData.entries());
+        setFormJson(formJson)
+    }
+
     const Component = switchT === '2D' ? Canvas_2D : Canvas_3D;
     const change = switchT === '2D' ? "3D" : "2D";
 
@@ -37,18 +52,12 @@ export const Sectors: React.FC = () => {
         <div className="sectors">
             <div className="sector">
                 {/* Left Sector Canvas */}
-                <Component data={data}/>
+                <Component data={data ?? []}/>
             </div>
             <div className="sector">
                 {/* Right Sector Fields, Inputs, Buttons, etc. */}
                 <SwitchButton text_switch={switchT} onClick={handleShowPopup}/>
-                <Field 
-                    title='Title' 
-                    placeholder='placeholder' 
-                    width={300} 
-                    data={data} 
-                    setData={setData}
-                />
+                <FieldsData handleSumbit={handleSumbit}/>
             </div>
             {showPopup && (
                 <Popup
