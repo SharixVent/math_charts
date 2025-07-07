b9d445ae0a7cf2c9d11435bfdc320d522a0c71de
Dawid Krawczyk <dawidkrawczyk@MacBook-Pro-Cocon.local>
2025-07-07 15:02:46 +0200

    Initialized start 3D project

--- src/components/canva-3d/Canva_3D.tsx
+++ src/components/canva-3d/Canva_3D.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+import type { Layout }  from 'plotly.js'
+
+const layout: Partial<Layout> = {
+  title: { text: '3D Spiral' },
+  autosize: true,
+  scene: {
+    xaxis: { title: {text: 'X'} },
+    yaxis: { title: {text: 'Y'} },
+    zaxis: { title: {text: 'Z'} },
+  },
+};
 
 export const Canvas_3D: React.FC = () => {
+  const linspaceFn = (startValue: number, stopValue: number, cardinality: number): number[] => {
+    const arr: number[] = [];
+    const step = (stopValue - startValue) / (cardinality - 1);
+    for (let i = 0; i < cardinality; i++) {
+      arr.push(parseFloat((startValue + step * i).toFixed(3)));
+    }
+    return arr;
+  };
 
-    return (
-        <>
-        3D
-        </>
-    );
+  const t = linspaceFn(0, 20, 100);
+  const x = t.map(i => Math.cos(i));
+  const y = t.map(i => Math.sin(i));
+  const z = t;
+
+  return (
+    <Plot
+      data={[
+        {
+          x,
+          y,
+          z,
+          mode: 'markers',
+          type: 'scatter3d',
+          marker: {
+            size: 12,
+            color: z,
+            colorscale: 'Viridis',
+            opacity: 0.8,
+          },
+        },
+      ]}
+      layout={layout}
+      style={{ width: '100%', height: '100%' }}
+      useResizeHandler={true}
+      config={{displaylogo: false}}
+    />
+  );
 };
 
 export default Canvas_3D;
